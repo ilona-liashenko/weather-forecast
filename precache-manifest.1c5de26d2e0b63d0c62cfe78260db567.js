ff68b837fd49ee74c9da0877e60b001d8d4255ea
ilona-liashenko <ilonka.firefly@gmail.com>
2024-06-18 14:36:21 +0300

    Updates

---
+++ precache-manifest.1c5de26d2e0b63d0c62cfe78260db567.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5074f12e2243ea973e87215410438faa",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "68def88de197c710d5dd",
+    "url": "/weather-forecast/static/css/main.4cc5f001.chunk.css"
+  },
+  {
+    "revision": "a3a8496de602eb523d4d",
+    "url": "/weather-forecast/static/js/2.b32d4e48.chunk.js"
+  },
+  {
+    "revision": "68def88de197c710d5dd",
+    "url": "/weather-forecast/static/js/main.2ba14977.chunk.js"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "a1cb48a64eeb44046ffc25f9be1e4a8a",
+    "url": "/weather-forecast/static/media/cloudy.a1cb48a6.png"
+  },
+  {
+    "revision": "2118a0dd4d863e2cfa8329195744da30",
+    "url": "/weather-forecast/static/media/fog.2118a0dd.png"
+  },
+  {
+    "revision": "7d524058dcbd1593bc32d95f2e9fb8cf",
+    "url": "/weather-forecast/static/media/rainy.7d524058.png"
+  },
+  {
+    "revision": "dc88578302a85b09a8842ce8b9087664",
+    "url": "/weather-forecast/static/media/snowy.dc885783.png"
+  },
+  {
+    "revision": "ff33f0de4f88c23349289bcb5798cd42",
+    "url": "/weather-forecast/static/media/sunny.ff33f0de.png"
+  },
+  {
+    "revision": "bb1c665c3a4638a131b795f9c5ee9750",
+    "url": "/weather-forecast/static/media/weather.bb1c665c.jpg"
+  }
+]);
